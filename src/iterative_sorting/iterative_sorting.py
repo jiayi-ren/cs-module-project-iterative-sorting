98a5dc8cac1eb6f4b9f5e34ee1082d6e84cb7ca0
Jiayi Ren <jiayiren22@gmail.com>
2020-07-20 09:58:22 -0700

    complete MVP: iterative sorting, searching

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,10 +7,13 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
@@ -18,7 +21,12 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    end_index = len(arr)
+    while end_index > 0:
+        for i in range(0, end_index-1):
+            if arr[i] > arr[i+1]:
+                arr[i+1], arr[i] = arr[i], arr[i+1]
+        end_index -= 1
 
     return arr
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    if len(arr) == 0:
+        return -1
+    
+    left = 0
+    right = len(arr) -1
+    while left < right:
+        pivot = int(left+ (right - left)/2)
+        if arr[pivot] > target:
+            right = pivot
+        if arr[pivot] < target:
+            left = pivot
+        if arr[pivot] == target:
+            return pivot
 
     return -1  # not found
